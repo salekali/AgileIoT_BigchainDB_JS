20ad5a37c2adc5de7c257dbeacb5d1d158869254
salekali <muhammadsalek.ali@studio.unibo.it>
2017-03-22 17:18:06 +0100

    read data from agile http api

--- index.js
+++ index.js
@@ -4,38 +4,89 @@
 var _func_utils = require('./func_utils');
 var XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
 
-// Generating Keypair
-var passcode = 'Thisisapassword'; //have to get password from user
-var agile_GW = new _func_utils.Ed25519Keypair(passcode);
-console.log(agile_GW.publicKey); // something like "DjPMHDD9JtgypDKY38mPz9f6owjAMAKhLuN1JfRAat8C"
-console.log(agile_GW.privateKey); // something like "7Gf5YRch2hYTyeLxqNLgTY63D9K5QH2UQ7LYFeBGuKvo"
 
+/******************
+* User Input Data *
+*******************/
+
+var agile_url = "http://192.168.8.60:8080/api/";// Input Agile-IoT API url
+var deviceId = "ble247189079500"; // Input deviceId
+var bdb_url = "http://localhost:9984/api/v1/"; // Input bigchaindb API endpoint
+var passcode = 'Thisisapassword'; // Input a text password for device here (minimum 8 characters)
+
+
+// API URLs to be used
+var url_device = agile_url+"device/"+deviceId+"/";
+var url_bdb = bdb_url+"transactions/";
+console.log(url_device, "\n" +url_bdb);
+
+
+
+/*********************
+* Generating Keypair *
+**********************/
+
+var agile_GW = new _func_utils.Ed25519Keypair(passcode);
+console.log("\n" + "The public key for this device is: " + agile_GW.publicKey + "\n");
+//console.log(agile_GW.privateKey);
+
+
+/*************************
+* Generating Transaction *
+**************************/
 
 // Generate condition for "CREATE" transaction and the output object for the transaction data model
 var agile_GW_Condition = new _func_utils.makeEd25519Condition(agile_GW.publicKey);
 var agile_GW_Output = new _func_utils.makeOutput(agile_GW_Condition);
 
 
-// Creating digital asset
-var reading = {
-    'name': 'dummy data',
-    'other': 'other data'
-};
-
-var metadata = null;
-
 // Generate "CREATE" transaction
+var reading = createAsset();
+var metadata = null;
 var createTx = (0, _func_utils.makeCreateTransaction)(reading, metadata, [agile_GW_Output], agile_GW.publicKey);
-//console.log(createTx);
 
-// Generate signed "CREATE" transaction
+// signing transaction with privateKey
 var signedCreateTx = (0, _func_utils.signTransaction)(createTx, agile_GW.privateKey);
-console.log(signedCreateTx);
+console.log("The tx id is " + signedCreateTx.id);
 
-// Send fulfilled "CREATE" transaction to the BigchainDB HTTP API endpoint
-var xhr = new XMLHttpRequest();
-var url = "http://localhost:9984/api/v1/transactions"; //figure out how to make this dynamic
-xhr.open("POST", url, true);
-xhr.setRequestHeader("Content-type", "application/json");
-var data = JSON.stringify(signedCreateTx);
-xhr.send(data);
+// Send signed transaction to bigchaindb
+sendTx();
+
+
+
+
+/*************************
+* Creating digital asset *
+**************************/
+
+function createAsset(){
+
+  // Get reading from Agile http api
+  var xhr0 = new XMLHttpRequest();
+  xhr0.withCredentials = true;
+  xhr0.open("GET", url_device, false);
+  xhr0.setRequestHeader("cache-control", "no-cache");
+  xhr0.send(null);
+  var data = xhr0.responseText;
+
+  console.log(data);
+  console.log(data);
+
+  return data;
+}
+
+
+
+/***************************************
+* POST transaction to BDB API endpoint *
+****************************************/
+
+function sendTx(){
+  // POST fulfilled "CREATE" transaction to the BigchainDB HTTP API endpoint
+  var xhr = new XMLHttpRequest();
+  var url = "http://localhost:9984/api/v1/transactions"; //figure out how to make this dynamic
+  xhr.open("POST", url, true);
+  xhr.setRequestHeader("Content-type", "application/json");
+  var data = JSON.stringify(signedCreateTx);
+  xhr.send(data);
+}
